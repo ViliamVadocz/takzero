72d506ab48827e1a75e6bd88957ce24a23942d49
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2023-07-16 23:00:21 +0200

    Create search crate

---
+++ search/src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::style)]
+#![warn(clippy::nursery)]
+// https://github.com/rust-lang/rust-clippy/issues/8538
+#![allow(clippy::iter_with_drain)]
+#![allow(warnings)]
+
