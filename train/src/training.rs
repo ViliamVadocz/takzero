e4c68f4ea2211d1e23bbfb4e1723edcd2bde9851
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2023-08-24 01:21:57 +0200

    Accumulate total loss and do backwards once

--- train/src/training.rs
+++ train/src/training.rs
@@ -54,22 +54,24 @@ pub fn run<const N: usize, const HALF_KOMI: i8, NET: Network + Agent<Game<N, HAL
     let mut training_steps = 0;
     let mut batches = 0;
 
+    let mut accumulated_total_loss = Tensor::zeros([1], (Kind::Float, device));
     while let Ok(batch) = rx.recv() {
         let batch_size = batch.len();
 
-        let mut inputs = Vec::new();
-        let mut policy_targets = Vec::new();
-        let mut value_targets = Vec::new();
-        let mut masks = Vec::new();
-        for target in batch {
-            inputs.push(game_to_tensor(&target.env, device));
-            policy_targets.push(policy_tensor::<N>(&target.policy, device));
-            value_targets.push(target.value);
-            masks.push(move_mask::<N>(
-                &target.policy.iter().map(|(m, _)| *m).collect::<Vec<_>>(),
-                device,
-            )); // FIXME: efficiency
-        }
+        let (input_and_value_targets, policy_target_and_masks): (Vec<_>, Vec<_>) = batch
+            .into_par_iter()
+            .map(|target| {
+                let input = game_to_tensor(&target.env, device);
+                let policy = policy_tensor::<N>(&target.policy, device);
+                let mask = move_mask::<N>(
+                    &target.policy.iter().map(|(m, _)| *m).collect::<Vec<_>>(),
+                    device,
+                );
+                ((input, target.value), (policy, mask))
+            })
+            .unzip();
+        let (inputs, value_targets): (Vec<_>, Vec<_>) = input_and_value_targets.into_iter().unzip();
+        let (policy_targets, masks): (Vec<_>, Vec<_>) = policy_target_and_masks.into_iter().unzip();
 
         // Get network output.
         let input = Tensor::cat(&inputs, 0);
@@ -89,15 +91,14 @@ pub fn run<const N: usize, const HALF_KOMI: i8, NET: Network + Agent<Game<N, HAL
         let loss_p = policy.kl_div(&p, Reduction::Sum, false) / i64::try_from(batch_size).unwrap();
         let loss_z = (z - values).square().mean(Kind::Float);
         println!("p={loss_p:?}\t z={loss_z:?}");
-        let total_loss = (loss_z + loss_p) / BATCHES_PER_STEP;
+        accumulated_total_loss += loss_z + loss_p;
 
         // Do multiple backwards batches before making a step.
-        total_loss.backward();
         batches += 1;
         if batches % BATCHES_PER_STEP == 0 {
             println!("Taking step!");
-            opt.step();
-            opt.zero_grad();
+            opt.backward_step(&(accumulated_total_loss / BATCHES_PER_STEP));
+            accumulated_total_loss = Tensor::zeros([1], (Kind::Float, device));
             training_steps += 1;
 
             if training_steps % STEPS_BETWEEN_PUBLISH == 0 {
