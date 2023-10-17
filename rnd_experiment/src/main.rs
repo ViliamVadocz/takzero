5f025ff3c707155cba89326d7d3277af5e7e4a00
Viliam Vadocz <viliam.vadocz@gmail.com>
2023-10-18 01:15:13 +0200

    Generate starting and unlikely positions for RND

--- rnd_experiment/src/main.rs
+++ rnd_experiment/src/main.rs
@@ -1,7 +1,8 @@
 use std::{
     collections::HashSet,
     fs::OpenOptions,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader, Write},
+    iter::repeat,
 };
 
 use charming::{
@@ -11,15 +12,21 @@ use charming::{
     Chart,
     HtmlRenderer,
 };
-use fast_tak::Game;
+use data::{ORIGINAL, STARTING, TEST, TRAINING, UNLIKELY};
+use fast_tak::{
+    takparse::{Color, Piece, Square, Tps},
+    Game,
+};
 use rand::{
     rngs::StdRng,
     seq::{IteratorRandom, SliceRandom},
+    Rng,
     SeedableRng,
 };
 use rayon::prelude::*;
 use takzero::{
     network::repr::{game_to_tensor, input_size},
+    search::env::Environment,
     target::Replay,
 };
 use tch::{
@@ -27,6 +34,8 @@ use tch::{
     Device,
     Tensor,
 };
+
+mod data;
 
 const N: usize = 5;
 const HALF_KOMI: i8 = 4;
@@ -67,12 +76,46 @@ fn improving_rnd(path: &nn::Path) -> nn::SequentialT {
 const DEVICE: Device = Device::Cuda(0);
 const REPLAY_LIMIT: usize = 2_000_000;
 const STARTING_REPLAY_BUFFER_SIZE: usize = 10_000;
-const NEW_REPLAYS_PER_STEP: usize = 10;
-const STEPS: usize = 4_000;
+const NEW_REPLAYS_PER_STEP: usize = 5;
+const STEPS: usize = 2_000;
 const SKIP: usize = 250;
 
 fn main() {
+    draw_chart(
+        TRAINING.to_owned(),
+        TEST.to_owned(),
+        ORIGINAL.to_owned(),
+        STARTING.to_owned(),
+        UNLIKELY.to_owned(),
+    );
+
+    experiment()
+}
+
+fn experiment() {
     let mut rng = StdRng::seed_from_u64(70);
+
+    let starting_positions: Vec<_> = (0..25 * 24)
+        .into_par_iter()
+        .map(|i| {
+            let mut actions = Vec::new();
+            let mut game = Env::default();
+            game.populate_actions(&mut actions);
+            let len = actions.len();
+            game.step(actions.drain(..).nth(i % len).unwrap());
+            game.populate_actions(&mut actions);
+            let len2 = actions.len();
+            game.step(actions.drain(..).nth((i / len) % len2).unwrap());
+            game
+        })
+        .collect();
+    let unlikely_positions: Vec<_> = (0..BATCH_SIZE)
+        .map(|_| generate_unlikely_position(&mut rng))
+        .collect();
+    for p in &unlikely_positions {
+        let tps: Tps = p.clone().into();
+        println!("{tps}");
+    }
 
     let file = OpenOptions::new()
         .read(true)
@@ -112,6 +155,8 @@ fn main() {
     let mut training_losses = Vec::new();
     let mut test_losses = Vec::new();
     let mut original_losses = Vec::new();
+    let mut starting_losses = Vec::new();
+    let mut unlikely_losses = Vec::new();
     for step in 0..STEPS {
         print!("step: {step}, ");
 
@@ -161,6 +206,52 @@ fn main() {
             original_losses.push(f32::try_from(loss).unwrap());
         });
 
+        // Test on starting
+        tch::no_grad(|| {
+            let batch = starting_positions.choose_multiple(&mut rng, BATCH_SIZE);
+            let inputs = Tensor::cat(
+                &batch
+                    .into_iter()
+                    .map(|env| game_to_tensor::<N, HALF_KOMI>(env, DEVICE))
+                    .collect::<Vec<_>>(),
+                0,
+            );
+
+            let actual = target
+                .forward_t(&inputs.set_requires_grad(false), false)
+                .detach();
+            let predicted = improving.forward_t(&inputs, false);
+            let loss = predicted
+                .mse_loss(&actual, tch::Reduction::None)
+                .sum_dim_intlist(1, false, None)
+                .mean(None);
+
+            starting_losses.push(f32::try_from(loss).unwrap());
+        });
+
+        // Test on impossible
+        tch::no_grad(|| {
+            let batch = unlikely_positions.choose_multiple(&mut rng, BATCH_SIZE);
+            let inputs = Tensor::cat(
+                &batch
+                    .into_iter()
+                    .map(|env| game_to_tensor::<N, HALF_KOMI>(env, DEVICE))
+                    .collect::<Vec<_>>(),
+                0,
+            );
+
+            let actual = target
+                .forward_t(&inputs.set_requires_grad(false), false)
+                .detach();
+            let predicted = improving.forward_t(&inputs, false);
+            let loss = predicted
+                .mse_loss(&actual, tch::Reduction::None)
+                .sum_dim_intlist(1, false, None)
+                .mean(None);
+
+            unlikely_losses.push(f32::try_from(loss).unwrap());
+        });
+
         // Training
         {
             println!("size of buffer: {}", simulated_buffer.len());
@@ -190,8 +281,47 @@ fn main() {
         }
     }
 
+    let mut file = std::fs::File::create("data.rs").unwrap();
+    #[rustfmt::skip]
+    file.write_all(
+        format!(
+            "pub const TRAINING: &[f32] = &{training_losses:?};\n\
+             pub const TEST: &[f32] = &{test_losses:?};\n\
+             pub const ORIGINAL: &[f32] = &{original_losses:?};\n\
+             pub const STARTING: &[f32] = &{starting_losses:?};\n\
+             pub const UNLIKELY: &[f32] = &{unlikely_losses:?};\n"
+        )
+        .as_bytes(),
+    )
+    .unwrap();
+
+    draw_chart(
+        training_losses,
+        test_losses,
+        original_losses,
+        starting_losses,
+        unlikely_losses,
+    );
+}
+
+fn draw_chart(
+    training_losses: Vec<f32>,
+    test_losses: Vec<f32>,
+    original_losses: Vec<f32>,
+    starting_losses: Vec<f32>,
+    unlikely_losses: Vec<f32>,
+) {
+    #[allow(clippy::iter_skip_zero)]
     let chart = Chart::new()
-        .title(Title::new().text("RND").left("center").top(0))
+        .title(
+            Title::new()
+                .text(format!(
+                    "RND, replays={STARTING_REPLAY_BUFFER_SIZE}+{NEW_REPLAYS_PER_STEP}n, \
+                     batch_size={BATCH_SIZE}"
+                ))
+                .left("center")
+                .top(0),
+        )
         .x_axis(Axis::new().name("training steps"))
         .y_axis(Axis::new().name("loss"))
         .series(
@@ -223,9 +353,106 @@ fn main() {
                     .map(|(x, y)| vec![x as f64, y as f64])
                     .collect(),
             ),
+        )
+        .series(
+            Line::new().dataset_id("starting").show_symbol(false).data(
+                starting_losses
+                    .into_iter()
+                    .enumerate()
+                    .skip(SKIP)
+                    .map(|(x, y)| vec![x as f64, y as f64])
+                    .collect(),
+            ),
+        )
+        .series(
+            Line::new().dataset_id("unlikely").show_symbol(false).data(
+                unlikely_losses
+                    .into_iter()
+                    .enumerate()
+                    .skip(SKIP)
+                    .map(|(x, y)| vec![x as f64, y as f64])
+                    .collect(),
+            ),
         );
 
     let mut renderer = HtmlRenderer::new("graph", 1000, 600).theme(Theme::Infographic);
     renderer.save(&chart, "graph.html").unwrap();
     println!("graph saved");
+}
+
+fn generate_unlikely_position(rng: &mut impl Rng) -> Env {
+    const MIN_FLATS: u8 = 10;
+    const MAX_FLATS: u8 = 30;
+    const MAX_WALLS: u8 = 3;
+    const MAX_PLIES: u16 = 200;
+
+    let mut env = Env::default();
+
+    // pick how many flats will be placed
+    let white = rng.gen_range(MIN_FLATS..=MAX_FLATS);
+    let black = rng.gen_range(MIN_FLATS..=MAX_FLATS);
+    let mut colors: Vec<_> = repeat(Color::White)
+        .take(white as usize)
+        .chain(repeat(Color::Black).take(black as usize))
+        .collect();
+    colors.shuffle(rng);
+
+    // place flats in stacks
+    for color in colors {
+        env.board
+            .get_mut(random_square(rng))
+            .unwrap()
+            .stack(Piece::Flat, color)
+            .expect("All stacks should have flats on top, so stacking is allowed");
+    }
+
+    // pick how many walls will be attempted to be placed
+    let white = rng.gen_range(0..=MAX_WALLS);
+    let black = rng.gen_range(0..=MAX_WALLS);
+    let mut colors: Vec<_> = repeat(Color::White)
+        .take(white as usize)
+        .chain(repeat(Color::Black).take(black as usize))
+        .collect();
+    colors.shuffle(rng);
+
+    // place walls
+    for color in colors {
+        let _ = env
+            .board
+            .get_mut(random_square(rng))
+            .unwrap()
+            .stack(Piece::Wall, color);
+    }
+
+    // place capstones
+    while {
+        for color in [Color::White, Color::Black] {
+            let _ = env
+                .board
+                .get_mut(random_square(rng))
+                .unwrap()
+                .stack(Piece::Cap, color);
+        }
+        rng.gen_bool(0.1) // small chance to generate more capstones
+    } {}
+
+    // randomize other data
+    env.ply = rng.gen_range(0..MAX_PLIES);
+    env.to_move = if rng.gen() {
+        Color::White
+    } else {
+        Color::Black
+    };
+    env.white_reserves.stones = rng.gen_range(0..=env.white_reserves.stones);
+    env.white_reserves.caps = rng.gen_range(0..=env.white_reserves.caps);
+    env.black_reserves.stones = rng.gen_range(0..=env.black_reserves.stones);
+    env.black_reserves.caps = rng.gen_range(0..=env.black_reserves.caps);
+
+    env
+}
+
+fn random_square(rng: &mut impl Rng) -> Square {
+    let row = rng.gen_range(0..N) as u8;
+    let col = rng.gen_range(0..N) as u8;
+    Square::new(col, row)
 }
