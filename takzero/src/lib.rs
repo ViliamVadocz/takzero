028c8001d7401c99c68adc12ed7ed6dafa6a331e
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2023-12-24 16:47:34 +0100

    Move lints to workspace toml file

--- takzero/src/lib.rs
+++ takzero/src/lib.rs
@@ -1,15 +1,3 @@
-#![warn(clippy::pedantic, clippy::style, clippy::nursery)]
-// https://github.com/rust-lang/rust-clippy/issues/8538
-#![allow(clippy::iter_with_drain)]
-// Just let me cast in peace
-#![allow(
-    clippy::cast_precision_loss,
-    clippy::cast_possible_truncation,
-    clippy::cast_possible_wrap
-)]
-// This one is just annoying
-#![allow(clippy::module_name_repetitions)]
-
 pub mod network;
 pub mod search;
 pub mod target;
