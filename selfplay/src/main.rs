0bbfba80aacd801a461eb674b1b6de063aa86705
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2023-12-30 18:44:59 +0100

    Decrease Dirichlet noise alpha

--- selfplay/src/main.rs
+++ selfplay/src/main.rs
@@ -40,7 +40,7 @@ const BATCH_SIZE: usize = 256;
 const VISITS: u32 = 800;
 const BETA: [f32; BATCH_SIZE] = [0.0; BATCH_SIZE];
 const WEIGHTED_RANDOM_PLIES: u16 = 30;
-const NOISE_ALPHA: f32 = 0.2;
+const NOISE_ALPHA: f32 = 0.05;
 const NOISE_RATIO: f32 = 0.1;
 const NOISE_PLIES: u16 = 20;
 
