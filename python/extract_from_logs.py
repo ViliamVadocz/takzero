0b95ee31e14deed3afcf820e64abdfa1afe448b9
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-01-22 01:11:42 +0100

    Extract data from new logs

--- python/extract_from_logs.py
+++ python/extract_from_logs.py
@@ -1,44 +1,55 @@
 import re
 import matplotlib.pyplot as plt
+import numpy as np
 
-RUN_NAMES = ["uncertainty0", "uncertainty1", "uncertainty2"]
-# RUN_NAMES = ["baseline0", "baseline1"]
+POLICY_PATTERN = re.compile(r"loss_policy = \[(\d+\.\d+)\]")
+VALUE_PATTERN = re.compile(r"loss_value = \[(\d+\.\d+)\]")
+UBE_PATTERN = re.compile(r"loss_ube = \[(\d+\.\d+)\]")
+RND_PATTERN = re.compile(r"loss_rnd = \[(\d+\.\d+)\]")
 
-RND_PATTERN = re.compile(r"rnd=\[(\d*\.\d*)\]")
-POLICY_PATTERN = re.compile(r"p=\[(\d*\.\d*)\]")
-VALUE_PATTERN = re.compile(r"z=\[(\d*\.\d*)\]")
-UBE_PATTERN = re.compile(r"u=\[(\d*\.\d*)\]")
-
-REPLAYS_PATTERN = re.compile(r"Adding (\d*) replays at (\d*) training steps")
-TARGETS_PATTERN = re.compile(r"Adding (\d*) targets at (\d*) training steps")
-
-
-def plot_cumulative(data):
-    cumulative = [(0, 0)] + [
-        (data[i][1], sum(row[0] for row in data[: i + 1])) for i in range(0, len(data))
-    ]
-    plt.plot(list(row[0] for row in cumulative), list(row[1] for row in cumulative))
+UBE_STATS_PATTERN = re.compile(
+    r"\[UBE STATS\] root: (\d+\.\d+), max: (\d+\.\d+), selected: (\d+\.\d+)"
+)
+BATCH_SIZE = 128
 
 
-for name in RUN_NAMES:
-    with open(f"_data/{name}.err", "r") as file:
-        contents = file.read()
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1 :] / n
 
-    # save data to files
-    with open(f"_data/{name}_rnd.txt", "w") as file:
-        file.writelines(x[1] + "\n" for x in re.finditer(RND_PATTERN, contents))
-    # with open(f"_data/{name}_policy.txt", "w") as file:
-    #     file.writelines(x[1] + "\n" for x in re.finditer(POLICY_PATTERN, contents))
-    # with open(f"_data/{name}_value.txt", "w") as file:
-    #     file.writelines(x[1] + "\n" for x in re.finditer(VALUE_PATTERN, contents))
-    # with open(f"_data/{name}_ube.txt", "w") as file:
-    #     file.writelines(x[1] + "\n" for x in re.finditer(UBE_PATTERN, contents))
 
-    replays = [(int(x[1]), int(x[2])) for x in re.finditer(REPLAYS_PATTERN, contents)]
-    plot_cumulative(replays)
-    targets = [(int(x[1]), int(x[2])) for x in re.finditer(TARGETS_PATTERN, contents)]
-    plot_cumulative(targets)
+# with open("_data/learn-9230002.err", "r") as file:
+#     contents = file.read()
+# policy_loss = list(float(x[1]) for x in re.finditer(POLICY_PATTERN, contents))
+# value_loss = list(float(x[1]) for x in re.finditer(VALUE_PATTERN, contents))
+# ube_loss = list(float(x[1]) for x in re.finditer(UBE_PATTERN, contents))
+# rnd_loss = list(float(x[1]) for x in re.finditer(RND_PATTERN, contents))
 
-plt.xlabel("steps")
-plt.ylabel("replays/targets")
-plt.savefig("interactions per step")
+# num_steps = len(ube_loss)
+# steps = list(range(num_steps))
+
+# plt.title("UBE loss")
+# plt.xlabel("training steps")
+# plt.ylabel("loss")
+# plt.plot(steps, ube_loss)
+# plt.ylim(0, 0.4)
+
+with open("_data/selfplay-9230004.err", "r") as file:
+    contents = file.read()
+ube_stats = [
+    (float(x[1]), float(x[2]), float(x[3]))
+    for x in re.finditer(UBE_STATS_PATTERN, contents)
+]
+root_ube = [t[0] for t in ube_stats]
+max_ube = [t[1] for t in ube_stats]
+selected_ube = [t[2] for t in ube_stats]
+
+plt.plot(moving_average(root_ube, n=2048), label="root")
+plt.plot(moving_average(max_ube, n=2048), label="max")
+plt.plot(moving_average(selected_ube, n=2048), label="selected")
+plt.ylabel("moving average UBE")
+plt.xlabel("self-play steps")
+plt.legend()
+
+plt.show()
