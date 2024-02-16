80c178a375c611830253572d0349b4240f6e9e0e
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-02-16 01:42:14 +0100

    Compare elo of no-explore run

--- python/elo.py
+++ python/elo.py
@@ -4,94 +4,90 @@ from subprocess import Popen, PIPE
 
 @dataclass
 class MatchResult:
-    a: int
-    b: int
+    a: str
+    b: str
     wins: int
     losses: int
     draws: int
 
     @staticmethod
-    def from_line(line: str) -> "MatchResult":
+    def from_line(line: str, prefix: str) -> "MatchResult":
         [a, b, wins, losses, draws] = line.split(", ")
-        return MatchResult(int(a), int(b), int(wins), int(losses), int(draws))
+        return MatchResult(
+            f"{prefix}_{int(a)}",
+            f"{prefix}_{int(b)}",
+            int(wins),
+            int(losses),
+            int(draws),
+        )
 
 
-with open("match_results_exploration.csv") as f:
-    exploration = [MatchResult.from_line(line) for line in f.readlines()]
-    exploration_players = {a: i for (i, a) in enumerate({m.a for m in exploration})}
-with open("match_results_baseline.csv") as f:
-    baseline = [MatchResult.from_line(line) for line in f.readlines()]
-    l = len(exploration_players)
-    baseline_players = {a: i + l for (i, a) in enumerate({m.a for m in baseline})}
+def read_results(path: str, players: set[str], prefix: str) -> list[MatchResult]:
+    with open(path) as f:
+        matches = [MatchResult.from_line(line, prefix) for line in f.readlines()]
+        players |= {m.a for m in matches} | {m.b for m in matches}
+    return matches
+
+
+def add_players(proc, players: set[str]) -> dict[str, int]:
+    new_players = dict()
+    for i, p in enumerate(players):
+        proc.stdin.write(f"addplayer {p}\n")
+        new_players[p] = i
+    return new_players
+
+
+def add_matches(proc, matches: list[MatchResult], players: dict[str, int]):
+    for m in matches:
+        add_match(proc, m, players)
+
+
+def add_match(proc, match: MatchResult, players: dict[str, str]):
+    white = players[match.a]
+    black = players[match.b]
+    for _ in range(match.wins):
+        proc.stdin.write(f"addresult {white} {black} 2\n")
+    for _ in range(match.losses):
+        proc.stdin.write(f"addresult {white} {black} 0\n")
+    for _ in range(match.draws):
+        proc.stdin.write(f"addresult {white} {black} 1\n")
+
 
 proc = Popen(
     [".\\python\\bayeselo.exe"], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True
 )
 proc.stdin.write("prompt off\n")
 
-for p in exploration_players.keys():
-    proc.stdin.write(f"addplayer exploration_{p}\n")
-for p in baseline_players.keys():
-    proc.stdin.write(f"addplayer baseline_{p}\n")
+players = set()
+exploration = read_results("match_results_exploration.csv", players, "exploration")
+baseline = read_results("match_results_baseline.csv", players, "baseline")
+no_explore = read_results("match_results_no_explore.csv", players, "no_explore")
+print("read results")
 
+players = add_players(
+    proc,
+    players,
+    # {p for p in players if (x := int(p.split("_")[-1])) % 50_000 == 0 and x <= 200_000},
+)
 print("added players")
-
-for m in exploration:
-    white = exploration_players[m.a]
-    black = exploration_players[m.b]
-    for _ in range(m.wins):
-        proc.stdin.write(f"addresult {white} {black} 2\n")
-    for _ in range(m.losses):
-        proc.stdin.write(f"addresult {white} {black} 0\n")
-    for _ in range(m.draws):
-        proc.stdin.write(f"addresult {white} {black} 1\n")
-
-print("added exploration matches")
-
-for m in baseline:
-    white = baseline_players[m.a]
-    black = baseline_players[m.b]
-    for _ in range(m.wins):
-        proc.stdin.write(f"addresult {white} {black} 2\n")
-    for _ in range(m.losses):
-        proc.stdin.write(f"addresult {white} {black} 0\n")
-    for _ in range(m.draws):
-        proc.stdin.write(f"addresult {white} {black} 1\n")
-
-print("added baseline matches")
-
-comparison = [
-    (0, 35, 24, 5),
-    (50000, 20, 29, 15),
-    (100000, 25, 30, 9),
-    (150000, 29, 18, 17),
-    (200000, 22, 23, 19),
-]
-
-for i, b_wins, e_wins, draws in comparison:
-    proc.stdin.flush()
-    exploration = exploration_players[i]
-    baseline = baseline_players[i]
-    for i in range(b_wins):
-        if i % 2 == 0:
-            proc.stdin.write(f"addresult {baseline} {exploration} 2\n")
-        else:
-            proc.stdin.write(f"addresult {exploration} {baseline} 0\n")
-    for i in range(e_wins):
-        if i % 2 == 0:
-            proc.stdin.write(f"addresult {exploration} {baseline} 2\n")
-        else:
-            proc.stdin.write(f"addresult {baseline} {exploration} 0\n")
-    for i in range(draws):
-        if i % 2 == 0:
-            proc.stdin.write(f"addresult {baseline} {exploration} 1\n")
-        else:
-            proc.stdin.write(f"addresult {exploration} {baseline} 1\n")
-
-print("added comparison matches")
 
 # proc.stdin.write("players\n")
 # proc.stdin.write("results\n")
+# exit()
+
+add_matches(proc, exploration, players)
+add_matches(proc, baseline, players)
+add_matches(proc, no_explore, players)
+print("added intra-agent matches")
+
+with open("match_results_tournament.csv") as f:
+    for a, b, wins, losses, draws in (line.split(", ") for line in f.readlines()):
+        add_match(proc, MatchResult(a, b, int(wins), int(losses), int(draws)), players)
+print("added inter-agent matches")
+
+# proc.stdin.write("players\n")
+# proc.stdin.write("results\n")
+# exit()
 
 proc.stdin.write("elo\n")  # enter elo interface
 proc.stdin.write("mm\n")  # compute maximum-likelyhood elo
@@ -99,22 +95,29 @@ proc.stdin.write("ratings\n")  # print out ratings
 proc.stdin.write("x\n")  # leave elo interface
 out = proc.communicate(input="x\n")[0]  # close application
 
+print(out)
+
 import re
 
 elo = [
-    (m[1].split("_"), int(m[2]))
-    for m in re.finditer(re.compile(r"(\w+_\d+)\s+(-?\d+)"), out)
+    (m[1], int(m[2])) for m in re.finditer(re.compile(r"([\w_]+\d+)\s+(-?\d+)"), out)
 ]
 
-baseline = sorted([(int(digits), e) for (name, digits), e in elo if "baseline" in name])
+baseline = sorted(
+    [(int(name.split("_")[1]), e) for name, e in elo if "baseline" in name]
+)
 exploration = sorted(
-    [(int(digits), e) for (name, digits), e in elo if "exploration" in name]
+    [(int(name.split("_")[1]), e) for name, e in elo if "exploration" in name]
+)
+no_explore = sorted(
+    [(int(name.split("_")[2]), e) for name, e in elo if "no_explore" in name]
 )
 
 import matplotlib.pyplot as plt
 
 plt.plot([x[0] for x in baseline], [x[1] for x in baseline], label="baseline")
 plt.plot([x[0] for x in exploration], [x[1] for x in exploration], label="exploration")
+plt.plot([x[0] for x in no_explore], [x[1] for x in no_explore], label="no_explore")
 plt.grid()
 plt.legend()
 plt.xlabel("training steps")
--- python/get_match_results.py
+++ python/get_match_results.py
@@ -2,13 +2,12 @@ import re
 import sys
 from pathlib import Path
 
-# [2024-01-04T08:25:25Z INFO  evaluation] model_000500.ot vs. model_000300.ot: Evaluation { wins: 29, losses: 1, draws: 2 } 90.6%
 PATTERN = re.compile(
-    r"model_(\d\d\d\d\d\d)\.ot vs\. model_(\d\d\d\d\d\d)\.ot: Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
+    # r"model_(\d\d\d\d\d\d)\.ot vs\. model_(\d\d\d\d\d\d)\.ot: Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
+    r"([\w_]+)_(\d\d\d\d\d\d)\.ot vs\. ([\w_]+)_(\d\d\d\d\d\d)\.ot: Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
 )
 
-SAVE_FILE = "match_results.csv"
-STEP = 100
+SAVE_FILE = "match_results_tournament.csv"
 
 # Clear file
 with open(SAVE_FILE, "w"):
@@ -20,6 +19,7 @@ for path in files:
         contents = file.read()
     with open(SAVE_FILE, "a") as file:
         file.writelines(
-            f"{int(x[1]) - int(x[1]) % STEP}, {int(x[2]) - int(x[2]) % STEP}, {x[3]}, {x[4]}, {x[5]}\n"
+            # f"{int(x[1])}, {int(x[2])}, {x[3]}, {x[4]}, {x[5]}\n"
+            f"{x[1]}_{int(x[2])}, {x[3]}_{int(x[4])}, {x[5]}, {x[6]}, {x[7]}\n"
             for x in re.finditer(PATTERN, contents)
         )
