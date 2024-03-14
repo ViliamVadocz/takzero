5710a62181dab43fb2d9d583e5fdc397b767db88
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-03-14 10:31:33 +0100

    Commit match results to git

--- python/get_match_results.py
+++ python/get_match_results.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 PATTERN = re.compile(
     # r"model_(\d\d\d\d\d\d)\.ot vs\. model_(\d\d\d\d\d\d)\.ot: Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
-    # r"([\w_]+)_(\d\d\d\d\d\d)\.ot vs\. ([\w_]+)_(\d\d\d\d\d\d)\.ot: Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
-    r"(\w*) vs. (\w*): Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
+    r"([\w_-]+)_(\d\d\d\d\d\d)\.ot vs\. ([\w_-]+)_(\d\d\d\d\d\d)\.ot: Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
+    # r"(\w*) vs. (\w*): Evaluation { wins: (\d*), losses: (\d*), draws: (\d*) }"
 )
 
-# SAVE_FILE = "match_results.csv"
-SAVE_FILE = "match_results_tournament.csv"
+SAVE_FILE = "match_results.csv"
+# SAVE_FILE = "match_results_tournament.csv"
 
 # Clear file
 with open(SAVE_FILE, "w"):
@@ -22,7 +22,7 @@ for path in files:
     with open(SAVE_FILE, "a") as file:
         file.writelines(
             # f"{int(x[1])}, {int(x[2])}, {x[3]}, {x[4]}, {x[5]}\n"
-            # f"{x[1]}_{int(x[2])}, {x[3]}_{int(x[4])}, {x[5]}, {x[6]}, {x[7]}\n"
-            f"{x[1]}, {x[2]}, {x[3]}, {x[4]}, {x[5]}\n"
+            f"{x[1]}_{int(x[2])}, {x[3]}_{int(x[4])}, {x[5]}, {x[6]}, {x[7]}\n"
+            # f"{x[1]}, {x[2]}, {x[3]}, {x[4]}, {x[5]}\n"
             for x in re.finditer(PATTERN, contents)
         )
