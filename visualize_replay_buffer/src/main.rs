7be10fc705a22f72593b62559000844399d9edea
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2024-05-23 16:46:49 +0200

    Reduce code repetition in visualize_replay_buffer

--- visualize_replay_buffer/src/main.rs
+++ visualize_replay_buffer/src/main.rs
@@ -16,7 +16,7 @@ const ACTIONS: usize = 5;
 
 fn load_positions(all_positions: &mut HashSet<Env>, path: impl AsRef<Path>) -> HashMap<Env, u32> {
     let mut positions = HashMap::new();
-    for replay in get_replays(path).unwrap() {
+    for replay in get_replays(path).unwrap().take(250_000) {
         let mut env = replay.env;
         for action in replay.actions.into_iter().take(ACTIONS + 1) {
             let canonical = env.clone().canonical();
@@ -33,35 +33,46 @@ fn main() {
     let _process = args.next();
 
     let mut all_positions = HashSet::new();
-    let d0 = load_positions(&mut all_positions, "4x4_old_directed_00_replays.txt");
-    println!("done loading d0");
-    let d1 = load_positions(&mut all_positions, "4x4_old_directed_01_replays.txt");
-    println!("done loading d1");
-    let u0 = load_positions(&mut all_positions, "4x4_neurips_undirected_00_replays.txt");
-    println!("done loading u0");
+    let files = [
+        "neurips_directed_00",
+        "simhash_directed_00",
+        "neurips_undirected_00", // no sampling
+        "neurips_undirected_01", // sampling all
+        "neurips_undirected_03", // filtered sampling
+    ];
+    let positions: Vec<_> = files
+        .into_iter()
+        .map(|path| {
+            (
+                path,
+                load_positions(&mut all_positions, format!("4x4_{path}_replays.txt")),
+            )
+        })
+        .collect();
 
-    let mut positions: Vec<Env> = all_positions.into_iter().collect();
-    positions.sort_by_key(|p| Reverse(p.white_reserves.stones + p.black_reserves.stones));
+    // let mut positions: Vec<Env> = all_positions.into_iter().collect();
+    // positions.sort_by_key(|p| Reverse(p.white_reserves.stones +
+    // p.black_reserves.stones));
 
-    println!("There are {} unique positions.", positions.len());
-    #[allow(clippy::cast_sign_loss)]
-    let size = (positions.len() as f64).sqrt().ceil() as usize;
-    println!("The image will be {size}x{size}.");
+    // println!("There are {} unique positions.", positions.len());
+    // #[allow(clippy::cast_sign_loss)]
+    // let size = (positions.len() as f64).sqrt().ceil() as usize;
+    // println!("The image will be {size}x{size}.");
 
-    let mut pixel_data = vec![0u8; size * size * 3];
-    for (i, position) in positions.into_iter().enumerate() {
-        let r = if d0.contains_key(&position) { 255 } else { 0 };
-        let g = if d1.contains_key(&position) { 255 } else { 0 };
-        let b = if u0.contains_key(&position) { 255 } else { 0 };
+    // let mut pixel_data = vec![0u8; size * size * 3];
+    // for (i, position) in positions.into_iter().enumerate() {
+    //     let r = if d0.contains_key(&position) { 255 } else { 0 };
+    //     let g = if d1.contains_key(&position) { 255 } else { 0 };
+    //     let b = if u0.contains_key(&position) { 255 } else { 0 };
 
-        let offset = i * 3;
-        pixel_data[offset] = r;
-        pixel_data[offset + 1] = g;
-        pixel_data[offset + 2] = b;
-    }
+    //     let offset = i * 3;
+    //     pixel_data[offset] = r;
+    //     pixel_data[offset + 1] = g;
+    //     pixel_data[offset + 2] = b;
+    // }
 
-    let img = RgbImage::from_raw(size as u32, size as u32, pixel_data).unwrap();
-    img.save("test.png").unwrap();
+    // let img = RgbImage::from_raw(size as u32, size as u32, pixel_data).unwrap();
+    // img.save("test.png").unwrap();
 
     // ---
 
@@ -78,9 +89,7 @@ fn main() {
         queue.push_back(env.canonical());
     }
 
-    let mut layers_dir0 = vec![vec![]; ACTIONS];
-    let mut layers_dir1 = vec![vec![]; ACTIONS];
-    let mut layers_und0 = vec![vec![]; ACTIONS];
+    let mut layers: Vec<_> = positions.iter().map(|_| vec![vec![]; ACTIONS]).collect();
 
     let mut actions = Vec::new();
     while let Some(env) = queue.pop_front() {
@@ -90,14 +99,10 @@ fn main() {
             clone.step(a);
             let canonical = clone.canonical();
 
-            if let Some(x) = d0.get(&canonical) {
-                layers_dir0[(env.ply - 2) as usize].push(x);
-            }
-            if let Some(x) = d1.get(&canonical) {
-                layers_dir1[(env.ply - 2) as usize].push(x);
-            }
-            if let Some(x) = u0.get(&canonical) {
-                layers_und0[(env.ply - 2) as usize].push(x);
+            for (i, (_, hash)) in positions.iter().enumerate() {
+                if let Some(x) = hash.get(&canonical) {
+                    layers[i][(env.ply - 2) as usize].push(x);
+                }
             }
 
             if canonical.ply - 2 < ACTIONS as u16 {
@@ -106,18 +111,10 @@ fn main() {
         }
     }
 
-    println!("d0");
-    for (i, layer) in layers_dir0.into_iter().enumerate() {
-        println!("{}: {}", i + 2, layer.len());
-    }
-
-    println!("d1");
-    for (i, layer) in layers_dir1.into_iter().enumerate() {
-        println!("{}: {}", i + 2, layer.len());
-    }
-
-    println!("u0");
-    for (i, layer) in layers_und0.into_iter().enumerate() {
-        println!("{}: {}", i + 2, layer.len());
+    for (i, (name, _)) in positions.iter().enumerate() {
+        println!("{name}");
+        for (ii, layer) in layers[i].iter().enumerate() {
+            println!("{}: {}", ii + 2, layer.len());
+        }
     }
 }
