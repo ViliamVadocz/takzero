abfbde1b361d292281fd11adb9e0c7dee45ea537
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2024-06-11 14:43:11 +0200

    Plot uniqueness per chunk, but using the buffer up to that point

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -16,20 +16,18 @@ use takzero::{
 
 fn main() {
     let replays = [
-        "undirected_greedy",
-        "undirected_sampled",
-        "undirected_filtered",
-        // "rnd_sampled",
-        // "lcghash_filtered_00",
-        // "simhash_filtered_00",
-        "lcghash_filtered",
-        "simhash_filtered",
+        // "undirected",
+        "lcghash",
+        // "lcghash_larger_beta",
+        "lcghash_no_window",
+        // "lcghash_no_window_smaller_beta",
+        // "simhash_no_window_smaller_beta",
     ];
 
     let mut chart = Chart::new()
         .title(
             Title::new()
-                .text("Ratio of Unique Positions Seen During Training")
+                .text("Ratio of Unique Positions in Chunk Seen During Training")
                 .subtext("Accounting for Symmetries")
                 .left("center")
                 .top(0),
@@ -41,7 +39,7 @@ fn main() {
     for r in replays {
         chart = chart.series(
             Line::new()
-                .data(get_unique_positions(format!("4x4_{r}.txt")))
+                .data(get_unique_positions(format!("4x4_{r}_replays.txt")))
                 .name(r)
                 .symbol(Symbol::None),
         );
@@ -51,17 +49,25 @@ fn main() {
 }
 
 fn get_unique_positions(path: impl AsRef<Path>) -> Vec<Vec<f64>> {
-    let mut positions: HashMap<_, u64> = HashMap::new();
+    let mut positions: HashMap<_, u64> = HashMap::with_capacity(20_000_000);
     let mut points = vec![vec![0.0, 1.0]];
-    let mut i = 0;
+    let mut positions_count = 0;
+    let mut previous_positions_count = 0;
+    let mut previous_unique_positions_count = 0;
 
     for replay in get_replays::<N, HALF_KOMI>(path).unwrap() {
-        if i / 250_000 > points.len() {
-            println!("{i} positions");
+        if positions_count / 250_000 >= points.len() {
+            println!("{positions_count} positions");
+
+            let unique_positions = positions.keys().len();
             points.push(vec![
-                i as f64,
-                positions.keys().len() as f64 / positions.values().sum::<u64>() as f64,
+                positions_count as f64,
+                (unique_positions - previous_unique_positions_count) as f64
+                    / (positions_count - previous_positions_count) as f64,
             ]);
+            previous_positions_count = positions_count;
+            previous_unique_positions_count = unique_positions;
+
             // positions.clear();
         }
         if points.len() > 80 {
@@ -70,11 +76,11 @@ fn get_unique_positions(path: impl AsRef<Path>) -> Vec<Vec<f64>> {
 
         let mut env = replay.env;
         *positions.entry(env.clone() /* .canonical() */).or_default() += 1;
-        i += 1;
+        positions_count += 1;
         for action in replay.actions {
             env.step(action);
             *positions.entry(env.clone() /* .canonical() */).or_default() += 1;
-            i += 1;
+            positions_count += 1;
         }
     }
 
