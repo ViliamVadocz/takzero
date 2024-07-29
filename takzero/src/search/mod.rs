daa156f3dd92b30d8b4ee2d790fadacbe7c7bb83
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-07-29 23:35:07 +0200

    Remove unused STEP constant

--- takzero/src/search/mod.rs
+++ takzero/src/search/mod.rs
@@ -3,8 +3,6 @@ pub mod env;
 pub mod eval;
 pub mod node;
 
-// Steps for TD-learning.
-pub const STEP: usize = 5;
 // Discount, also known as gamma.
 pub const DISCOUNT_FACTOR: f32 = 0.997;
 pub const SERIES_DISCOUNT: f32 = 1.0 / (1.0 - DISCOUNT_FACTOR * DISCOUNT_FACTOR);
