7e5068a889bb6209f117eda3ae10615374026abf
Viliam Vadocz <viliam.vadocz@gmail.com>
2024-08-26 15:16:00 +0200

    Plot 6x6 replay buffer uniqueness

--- graph/src/main.rs
+++ graph/src/main.rs
@@ -9,13 +9,13 @@ use charming::{
     HtmlRenderer,
 };
 use takzero::{
-    network::net4_rnd::{HALF_KOMI, N},
+    network::net6_simhash::{HALF_KOMI, N},
     search::env::Environment,
     target::get_replays,
 };
 
 fn main() {
-    let replays = ["undirected_02", "simhash_02", "naive_00"];
+    let replays = ["undirected", "naive", "directed"];
 
     let mut chart = Chart::new()
         .title(
@@ -30,7 +30,7 @@ fn main() {
         .grid(Grid::new())
         .legend(Legend::new().data(replays.to_vec()).bottom(10).left(10));
     for r in replays {
-        let data = get_unique_positions(format!("_replays/{r}.txt"));
+        let data = get_unique_positions(format!("./{r}_replays.txt"));
         println!("{r} = [");
         for p in &data {
             println!("    ({}, {}),", p[0], p[1]);
@@ -43,28 +43,40 @@ fn main() {
 }
 
 fn get_unique_positions(path: impl AsRef<Path>) -> Vec<Vec<f64>> {
-    let mut positions: HashMap<_, u64> = HashMap::with_capacity(20_000_000);
+    const POSITIONS: usize = 10_000_000;
+    const POINT_RATE: usize = 250_000;
+    const POINTS: usize = POSITIONS / POINT_RATE;
+
+    let mut positions: HashMap<_, u64> = HashMap::with_capacity(POSITIONS);
     let mut points = vec![vec![0.0, 1.0]];
     let mut positions_count = 0;
     let mut previous_positions_count = 0;
     let mut previous_unique_positions_count = 0;
 
     for replay in get_replays::<N, HALF_KOMI>(path).unwrap() {
-        if positions_count / 250_000 >= points.len() {
-            // println!("{positions_count} positions");
+        if positions_count / POINT_RATE >= points.len() {
+            let position_count_diff = positions_count - previous_positions_count;
 
             let unique_positions = positions.keys().len();
+            // Yaniv Metric
             points.push(vec![
                 positions_count as f64,
                 (unique_positions - previous_unique_positions_count) as f64
-                    / (positions_count - previous_positions_count) as f64,
+                    / position_count_diff as f64,
             ]);
+            // Will Metric
+            // points.push(vec![
+            //     positions_count as f64,
+            //     unique_positions as f64 / position_count_diff as f64,
+            // ]);
+
             previous_positions_count = positions_count;
             previous_unique_positions_count = unique_positions;
 
+            // Uncomment if using Will Metric.
             // positions.clear();
         }
-        if points.len() > 160 {
+        if points.len() > POINTS {
             break;
         }
 
