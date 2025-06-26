14194b8bb931c37724355025b64f97fe10e1fb88
Viliam Vadocz <v.vadocz@student.tudelft.nl>
2025-06-26 17:13:06 +0200

    Some old change which I do not remember, amazing commit, yay :)

--- analysis/src/main.rs
+++ analysis/src/main.rs
@@ -5,28 +5,20 @@ use std::{
 
 use clap::Parser;
 use fast_tak::takparse::{Move, Tps};
-use rand::prelude::*;
 use takzero::{
     network::{
         net6_simhash::{Env, Net},
-        repr::game_to_tensor,
-        HashNetwork,
         Network,
     },
-    search::{
-        env::Environment,
-        node::{batched::BatchedMCTS, Node},
-    },
+    search::{env::Environment, node::Node},
 };
-use tch::Device;
 
-const DEVICE: Device = Device::Cuda(0);
 const BETA: f32 = 0.0;
-// const BATCH_SIZE: usize = 128;
+const BATCH_SIZE: usize = 128;
 
 #[derive(Parser, Debug)]
 struct Args {
-    /// Path to model to load.
+    /// Path to model to load
     #[arg(long)]
     model_path: PathBuf,
     /// Run an example game
@@ -37,114 +29,34 @@ struct Args {
     tps: Option<Tps>,
 }
 
-// #[allow(unused)]
-// fn gather_policy_data(agent: &Net, rng: &mut impl Rng) {
-//     let file = OpenOptions::new().read(true).open("replays.txt").unwrap();
-//     let env_batches = BufReader::new(file)
-//         .lines()
-//         .filter_map(|line| line.ok()?.parse::<Replay<Env>>().ok())
-//         .choose_multiple(rng, 1024)
-//         .into_iter()
-//         .map(|mut replay| {
-//             replay.advance(rng.gen_range(0..replay.len()));
-//             replay.env
-//         })
-//         .array_chunks::<BATCH_SIZE>()
-//         .collect::<Vec<_>>();
-
-//     let mut line = String::new();
-//     for envs in env_batches {
-//         let mut batched_mcts = BatchedMCTS::from_envs(envs);
-
-//         // for _ in 0..800 {
-//         //     batched_mcts.simulate(&agent, &[BETA; 128]);
-//         // }
-//         batched_mcts.gumbel_sequential_halving(agent, &[BETA; 128], 64, 768,
-// rng);
-
-//         for (node, _) in batched_mcts.nodes_and_envs() {
-//             line.clear();
-//             node.children.iter().for_each(|(a, child)| {
-//                 write!(
-//                     &mut line,
-//                     "{a}:{}:{}:{}:{},",
-//                     child.visit_count, child.evaluation, child.std_dev,
-// child.logit                 )
-//                 .unwrap();
-//             });
-//             println!("{line}");
-//         }
-//     }
-// }
+fn run_example(mut env: Env, mut node: Node<Env>, agent: &Net) {
+    while env.terminal().is_none() {
+        println!("tps: {}", Tps::from(env.clone()));
+        node.simulate_batch(agent, &env, BETA, BATCH_SIZE);
+        let action = node.select_best_action();
+        println!(">>> {action}");
+        node.descend(&action);
+        env.step(action);
+    }
+}
 
 fn main() {
+    env_logger::init();
     let args = Args::parse();
-    let agent = Net::load_partial(args.model_path, DEVICE).unwrap();
-    let mut rng = StdRng::seed_from_u64(123);
+    let device = tch::Device::cuda_if_available();
+    if !device.is_cuda() {
+        log::warn!("CUDA not available, using CPU");
+    }
+    let agent = Net::load_partial(args.model_path, device).unwrap();
 
     let mut env = args.tps.map(Env::from).unwrap_or_default();
     let mut node = Node::default();
     if args.example {
-        while env.terminal().is_none() {
-            println!("tps: {}", Tps::from(env.clone()));
-            // for _ in 0..visits {
-            //     node.simulate_simple(&agent, env.clone(), BETA);
-            // }
-            let mut batched_mcts = BatchedMCTS::from_envs([env.clone()]);
-            let (bm_node, _) = batched_mcts.nodes_and_envs_mut().next().unwrap();
-            std::mem::swap(bm_node, &mut node);
-            batched_mcts.gumbel_sequential_halving(&agent, &[BETA], 64, 768, &mut rng);
-            let (bm_node, _) = batched_mcts.nodes_and_envs_mut().next().unwrap();
-            std::mem::swap(bm_node, &mut node);
-            println!("{node}");
-
-            // Print raw network output.
-            let xs = tch::Tensor::concat(
-                &node
-                    .children
-                    .iter()
-                    .map(|(a, _)| {
-                        let mut clone = env.clone();
-                        clone.step(*a);
-                        game_to_tensor(&clone, tch::Device::Cpu)
-                    })
-                    .collect::<Vec<_>>(),
-                0,
-            )
-            .to(DEVICE);
-            let local_unc: Vec<f32> = agent.forward_hash(&xs).try_into().unwrap();
-            let (_policy, value, ube) = agent.forward_t(&xs, false);
-            let value_out: Vec<Vec<f32>> = value.try_into().unwrap();
-            let ube_out: Vec<Vec<f32>> = ube.exp().try_into().unwrap();
-
-            println!("network output:");
-            println!("[action]  [value]  [local]  [ ube ]");
-            let mut network_output = local_unc
-                .into_iter()
-                .zip(value_out)
-                .zip(ube_out)
-                .zip(node.children.iter())
-                .collect::<Vec<_>>();
-            network_output.sort_by_key(|(_, (_, n))| n.visit_count);
-            network_output.reverse();
-            for (((local, value), ube), (action, _)) in network_output {
-                println!(
-                    "{: ^8}  {:+.4}  {local:+.4}  {:+.4}",
-                    action.to_string(),
-                    value[0],
-                    ube[0]
-                );
-            }
-            println!();
-
-            let action = node.select_best_action();
-            println!(">>> {action}");
-            node.descend(&action);
-            env.step(action);
-        }
-        return;
+        return run_example(env, node, &agent);
     }
 
+    // TODO: History
+    // let mut history = vec![];
     let mut input = String::new();
     loop {
         input.clear();
@@ -163,17 +75,7 @@ fn main() {
             }
             node.descend(&mov);
         } else {
-            // let visits: u32 = trim.parse().unwrap_or(1);
-            // println!("simulating {visits} visits");
-            // for _ in 0..visits {
-            //     node.simulate_simple(&agent, env.clone(), BETA);
-            // }
-            let mut batched_mcts = BatchedMCTS::from_envs([env.clone()]);
-            let (bm_node, _) = batched_mcts.nodes_and_envs_mut().next().unwrap();
-            std::mem::swap(bm_node, &mut node);
-            batched_mcts.gumbel_sequential_halving(&agent, &[BETA], 64, 768, &mut rng);
-            let (bm_node, _) = batched_mcts.nodes_and_envs_mut().next().unwrap();
-            std::mem::swap(bm_node, &mut node);
+            node.simulate_batch(&agent, &env, BETA, BATCH_SIZE);
         }
         println!("{node}");
     }
